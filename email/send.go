4e1d965ade4d8f057f187f0daaf232d8824d4df6
bujosa <davidbujosa@gmail.com>
2023-10-29 22:14:11 -0400

    feat: implement email package

---
+++ email/send.go
@@ -0,0 +1,19 @@
+package email
+
+import (
+	"fmt"
+	"os"
+
+	"gopkg.in/gomail.v2"
+)
+
+func SendEmail(m *gomail.Message) {
+	d := gomail.NewDialer("smtp-relay.brevo.com", 587, os.Getenv("SMTP_USER"), os.Getenv("SMTP_PASSWORD"))
+
+	// Send the email to Bob, Cora and Dan.
+	if err := d.DialAndSend(m); err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Email sent successfully!")
+}
